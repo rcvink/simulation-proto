96aa16ffdd9e3b681edf15106332e41fbf37187d
rcvink <rrvink@gmail.com>
2020-02-23 12:17:04 +0000

    disable slow incrementer while waiting

--- src/SlowIncrementer.svelte
+++ src/SlowIncrementer.svelte
@@ -3,15 +3,18 @@
 	export let time = 0;
 	export let label;
 
-	let promise = slow(() => { }, time);
+	let disabled = false;
+	let promise = slow(() => { }, () => { }, 0);
 
 	function onClick() {
-		promise = slow(increment, time);
+		disabled = true;
+		promise = slow(increment, enable, time);
 	}
 	
-	async function slow(fn, ms) {
+	async function slow(fn1, fn2, ms) {
 		await timeout(ms);
-		fn(ms);
+		fn1(ms);
+		fn2();
 	}
 
 	function timeout(ms) {
@@ -22,10 +25,15 @@
 	function increment() {
 		count.update(n => n + 1);
 	}
+
+	function enable() {
+		disabled = false;
+	}
+
 </script>
 
-<div>
-	<button on:click={onClick}>
-		+ Add {label}
-	</button>
+<div>/
+		<button on:click={onClick} disabled="{disabled}">
+			+ Add {label}
+		</button>
 </div>
