68d639217442b1f0fab9dc956b875b2d5439f218
rcvink <rrvink@gmail.com>
2020-02-24 08:52:07 +0000

    set default ram and cpu to 1

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
-export const memoryCount = writable(0);
-export const cpuCount = writable(0);
+export const memoryCount = writable(1);
+export const cpuCount = writable(1);
 export const storageCount = writable(0);
 export const networkCount = writable(0);
 export const haikuCount = writable(0);
