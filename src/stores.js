724fdb9057169d55309a6b35bcacd6c7b63eb3be
rcvink <rrvink@gmail.com>
2020-02-29 09:58:52 +0000

    set storageCount to 1 on start

--- src/stores.js
+++ src/stores.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 
 export const memoryCount = writable(1);
 export const cpuCount = writable(1);
-export const storageCount = writable(0);
+export const storageCount = writable(1);
 export const networkCount = writable(0);
 export const haikuCount = writable(0);
 export const cryptoCount = writable(0);
