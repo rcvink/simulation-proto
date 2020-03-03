182db80ed51587738eb54c4fbf5cf86ce9911af0
rcvink <rrvink@gmail.com>
2020-03-03 19:37:57 +0000

    default storageCount to be 100

--- src/stores.js
+++ src/stores.js
@@ -3,7 +3,7 @@ import { writable, readable } from 'svelte/store';
 
 export const memoryCount = writable(1);
 export const cpuCount = writable(1);
-export const storageCount = writable(1);
+export const storageCount = writable(100);
 export const networkCount = writable(0);
 export const haikuCount = writable(0);
 export const cryptoCount = writable(0);
