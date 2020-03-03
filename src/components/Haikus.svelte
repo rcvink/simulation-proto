31cb069e5ed210977c363213bdbaf32e94d3db69
rcvink <rrvink@gmail.com>
2020-03-03 18:26:13 +0000

    scroll down haikus when adding new, style haikus

--- src/components/Haikus.svelte
+++ src/components/Haikus.svelte
@@ -1,21 +1,40 @@
 <script>
     import Haiku from './Haiku.svelte';
+    import { onMount, tick } from 'svelte';
 
     export let allHaikus;
     export let haikuCount;
     let haikus = [];
-    const maxHaikus = 3;
+    let div;
 
-    const getRandom = () => 
+    const addNewHaiku = async (n) => {
+        if (n > 0) {
+            haikus.push(getRandomHaiku());
+            haikus = haikus;
+            await tick();
+            scrollDown();
+        }
+    }
+    
+    const getRandomHaiku = () => 
         allHaikus[Math.floor(Math.random() * Math.floor(allHaikus.length))];
 
-    haikuCount.subscribe(n => {
-        if (n > 0) {
-            haikus.push(getRandom());
-            haikus = haikus;
-        }
+    const scrollDown = () => 
+        div.scrollTop = div.scrollHeight;
+    
+    onMount(() => {
+        div = document.getElementById('haikus');
+        haikuCount.subscribe(addNewHaiku);
     });
 </script>
+
+<style>
+#haikus {
+    overflow-y: auto;
+    max-height: 245px;
+    min-width: 340px;
+}
+</style>
 
 <div id="haikus">
     {#each haikus as haiku}
