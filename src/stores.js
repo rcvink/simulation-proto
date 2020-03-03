0bb48b2d7d073fbe4863a0d13cd49bf6b845bed8
rcvink <rrvink@gmail.com>
2020-03-03 14:38:16 +0000

    import haikus from json and populate store

--- src/stores.js
+++ src/stores.js
@@ -1,4 +1,5 @@
-import { writable } from 'svelte/store';
+import { haikus } from './haikus.json';
+import { writable, readable } from 'svelte/store';
 
 export const memoryCount = writable(1);
 export const cpuCount = writable(1);
@@ -7,3 +8,4 @@ export const networkCount = writable(0);
 export const haikuCount = writable(0);
 export const cryptoCount = writable(0);
 export const isCryptoEnabled = writable(false);
+export const allHaikus = readable(haikus);
