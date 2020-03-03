71b3c788e354e9b9db5797f40e79932c23a0ef30
rcvink <rrvink@gmail.com>
2020-03-03 00:03:39 +0000

    increment at correct rate/interval, increment to max correctly

--- src/components/LinearIncrementer.svelte
+++ src/components/LinearIncrementer.svelte
@@ -1,53 +1,34 @@
 <script>
-    export let isEnabled;
-    export let enabledText;
-    export let disabledText;
-    export let count;
-    export let rate;
-    export let incrementBy;
-    export let maxCount;
-    
-    function onClick() {
-        isEnabled.update(n => n = !n);
-        onEnable();
+  export let isEnabled;
+  export let enabledText;
+  export let disabledText;
+  export let count;
+  export let rate;
+  export let incrementBy;
+  export let maxCount;
+  let interval;
+
+  const onClick = () => 
+    isEnabled.update(n => !n);
+
+  const increment = () => 
+    count.update(n => Math.min(n + incrementBy, $maxCount));
+
+  isEnabled.subscribe(enabled => {
+    if (enabled) {
+      interval = setInterval(increment, 1000 / rate)
+    } else {
+      clearInterval(interval);
     }
-
-    async function onEnable() {
-        while (shouldIncrement()) {
-            await timeout(1000 / rate);
-            count.update(n => n + incrementBy);
-        }
-    }
-
-    function timeout(ms) {
-		return new Promise(resolve => 
-			setTimeout(resolve, ms));
-    }
-
-    function shouldIncrement() {
-        return $isEnabled && $count + incrementBy <= $maxCount; 
-    }
-
-    maxCount.subscribe(() => {
-        if (shouldIncrement()) {
-            onEnable();
-        }
-    });
-
-    count.subscribe((s, s2) => {
-        console.log("s", s, "s2", s2);
-       if (shouldIncrement()) {
-            onEnable();
-        }
-    });
+  });
 </script>
 
 <div>
-    <button on:click={onClick}>
-        {#if $isEnabled}
-            {enabledText}
-        {:else}
-            {disabledText}
-        {/if}
-    </button>
+  <button on:click={onClick}>
+      {#if $isEnabled}
+          {enabledText}
+      {:else}
+          {disabledText}
+      {/if}
+  </button>
 </div>
