5b3a88576c04944a180dc80041ac8467acbe1878
rcvink <rrvink@gmail.com>
2020-03-03 14:38:32 +0000

    add Haiku component for formatting and displaying haiku

---
+++ src/components/Haiku.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let text;
+	const linesCount = 3;
+	const lines = text.split(`\n`);
+</script>
+
+<style>
+ul {
+	list-style-type: none;
+}
+</style>
+
+<ul>
+	{#each lines as line}	
+	<li>
+		{line}
+	</li>
+	{/each}
+</ul>
