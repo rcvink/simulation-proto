500a0c7d5cc424b933f9b5fa4d0dcab7399c528f
rcvink <rrvink@gmail.com>
2020-03-27 11:56:31 +0000

    update font color on time change, round time up to nearest int

--- src/components/HaikuTime.svelte
+++ src/components/HaikuTime.svelte
@@ -1,38 +1,32 @@
 <script>
-    import { onMount, tick } from 'svelte';
-
     export let time;
-
     let severity = 'low';
 
-    const getClass = () => {
-        if (time < 1000) {
-            return 'low';
-        } else if (time < 5000) {
-            return 'medium';
-        } else if (time < 10000) {
-            return 'high';
+    time.subscribe(newTime => {
+        if (newTime <= 3000) {
+            severity = 'low';
+        } else if (newTime <= 10000) {
+            severity = 'medium';
+        } else {
+            severity = 'high';
         }
-        else {
-            return 'very-high';
-        }
-    }
+    });
 </script>
 
 <style>
     .low {
-        background-color: green;
+        color: green;
     }
 
     .medium {
-        background-color: green;
+        color: orange;
     }
 
     .high {
-        background-color: green;
+        color: red;
     }
 </style>
 
-<div class={getClass()}>
-{time}
-</div>
+<span class={severity}>
+{Math.ceil($time)}
+</span>
