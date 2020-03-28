98caa2498eb365ac0875e78707d8d2cd60b6b188
rcvink <rrvink@gmail.com>
2020-03-28 12:22:07 +0000

    wip cancel haiku think

--- src/components/SlowIncrementer.svelte
+++ src/components/SlowIncrementer.svelte
@@ -8,36 +8,30 @@
 
 	const onClick = () => {
 		if (isThinking) {
-			console.log(isThinking);
+			isThinking = false;
+			promise = null;
 		} else {
 			promise = think(
-				increment, 
-				() => isThinking = false, 
-				time, 
-				() => isThinking = false);
+				() => isThinking = true,
+				() => isThinking = false,
+				increment,
+				time);
 		}
 	}
 
-	const stopThink = async (fn) => {
-		await promise.resolve()
-	}
-	
-	const think = async (afterFn1, afterFn2, ms, cancelFn) => {
-		await timeout(ms, cancelFn);
-		afterFn1(ms);
+
+	const think = async (beforeFn, afterFn1, afterFn2, ms) => {
+		beforeFn();
+		await timeout(ms);
+		afterFn1();
 		afterFn2();
 	}
 
-	const timeout = (ms, cancelFn) =>
-		new Promise(
-			resolve => setTimeout(resolve, ms), 
-			reject => cancelFn());
+	const timeout = (ms) =>
+		new Promise(resolve => setTimeout(resolve, ms));
 
 	const increment = () => 
 		count.update(n => n + 1);
-
-	const setThinkingFalse = () => 
-		isThinking = false;
 </script>
 
 <div>
